0033733679dbd3b9b0dae85e66a5e4f349abaf6f
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-11-06 15:04:41 -0600

    website 2

--- website1/contact.php
+++ website1/contact.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My PHP Website</title>
-</head>
-<body>
-
-<ul>
-<li><a href='index.php'>Home</a></li>
-<li><a href='about.php'>About</a></li>
-<li><a href='contact.php'>Contact</a></li>
-</ul>
+<?php include 'inc/header.php'; ?>
 
 <h1>Contact</h1>
 
-<footer>
-
-<p>My PHP Website &copy; 2020</p>
-
-</footer>
-
-</body>
-</html>
+<?php include 'inc/footer.php'; ?>
